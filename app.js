36c42b69d559a9f0fc1065fc95ec76181e616e91
Szabolcs Gergye <szabolcs.gergye@attrecto.com>
2018-02-21 22:21:29 +0100

    error handling

--- app.js
+++ app.js
@@ -6,7 +6,28 @@ global.rootRequired = (name) => {
 
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    console.log(err, err.message, err.stack);
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
 
 module.exports = app;
