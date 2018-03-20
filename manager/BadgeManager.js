c715d70b138dc513c89eaed2d7f52fabf46d6cea
Péter Kőhalmi <peter.kohalmi@attrecto.com>
2018-03-20 14:39:02 +0100

    badge manager skeleton

---
+++ manager/BadgeManager.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const database = rootRequired('database');
+const path = require('path');
+const dbPath = path.resolve(__dirname, '..', 'database', 'education.db');
+
+const getBadges = async (message) => {};
+
+const createBadge = async (message) => {};
+
+const getBadge = async (message) => {};
+
+const updateBadge = async (message) => {};
+
+const deleteBadge = async (message) => {};
+
+module.exports = {
+    getBadges,
+    getBadge,
+    createBadge,
+    updateBadge,
+    deleteBadge
+};
