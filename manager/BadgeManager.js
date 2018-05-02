c4740500cfc01faa1fba319b2a0e642598a379b7
Péter Kőhalmi <peter.kohalmi@attrecto.com>
2018-05-02 15:53:04 +0200

    added badge patch logic

--- manager/BadgeManager.js
+++ manager/BadgeManager.js
@@ -54,7 +54,44 @@ const getBadge = async (message) => {
   return badge;
 };
 
-const updateBadge = async (message) => {};
+const updateBadge = async (message) => {
+  const {
+    id,
+    data: updateData
+  } = message;
+
+  await database.open(dbPath);
+
+  const query = "SELECT * FROM badges WHERE id = ?";
+  const badge = await database.get(query, [id]);
+
+  if (!badge) {
+    throw new AppError(404, 'Badge not found!')
+  }
+
+  //check name
+  const name = updateData.name;
+  let checkBadgeQuery = `SELECT id, name, description FROM badges WHERE name = ?`;
+  const foundBadge = await database.get(checkBadgeQuery, [name]);
+  if (foundBadge) {
+    throw new AppError(400, "Badge already exists with this name.");
+  }
+
+  Object.assign(badge, {
+    name: typeof updateData.name === 'string' && updateData.name.length ? updateData.name : badge.name,
+    description: typeof updateData.description  === 'string' ? updateData.description : badge.description
+  });
+
+  const updateBadgeQuery = `UPDATE badges SET name = ?, description = ? WHERE id = ?`;
+
+  await database.runWithPrepareStatement(updateBadgeQuery, [
+    badge.name,
+    badge.description,
+    id
+  ]);
+
+  return badge;
+};
 
 const deleteBadge = async (message) => {};
 
